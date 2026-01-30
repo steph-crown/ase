fb1f035c2209de2a1dd7825d213a9385df4cdb9c
steph-crown <emmanuelstephen024@gmail.com>
2026-01-30 18:26:53 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,9 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = "".to_string();
+
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
